1786685883359e60029f45f3193e60414eb9a54c
James Taylor <jostylr@gmail.com>
2013-01-08 15:57:28 -0500

    lp3_5 is the version that we add in common directives and other goodies. its compiled script is litpro.js and it relies on the node_modules/literate-programing.js
    The 3_5 version strippd out the file compiling from core functionality to set the foundation for use in browser editor version.

---
+++ litpro.js
@@ -0,0 +1,23 @@
+/*global process, require, console*/
+var fs = require('fs');
+var lp = require('literate-programming');
+var save = function (files, dir) {
+    if (dir) {
+        process.chdir(dir);
+    }
+    for (var name in files) {
+        // name, text
+      console.log(name + " saved");
+      fs.writeFileSync(name, files[name][0], 'utf8');
+    }
+};
+var filename = process.argv[2];
+if (!filename) {
+    console.log("Usage: litpro file-to-compile optional:directory-to-place-result");
+    process.exit();
+}
+var dir = process.argv[3];
+var md = fs.readFileSync(filename, 'utf8');
+var doc = lp.compile(md);
+save(doc.compiled, dir); 
+console.log(doc.log.join("\n\n"));
